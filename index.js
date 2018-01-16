3f54663209a0adc73e916eaed8b0a542d1b467fc
Ashley Ryan <ashleyskyeryan@gmail.com>
2018-01-16 22:58:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,4 +16,11 @@ function logWhisper(string) {
 
 function sayHiToGrandma(string) {
   if (string === string.toLowerCase)
+  return "I can't hear you!"
+
+  elseif (string === string.toUpperCase)
+  return "YES INDEED!"
+
+  ELSE (string === "I love you, Grandma.")
+  return "I love you, too."
 }
